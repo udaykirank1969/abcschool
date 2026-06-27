7a91d1748351d06a88038dc7f217b76ac334a765
K Uday Kiran <udaykirank74@gmail.com>
2026-06-28 00:45:24 +0530

    First website commit

---
+++ website/js/script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+});
