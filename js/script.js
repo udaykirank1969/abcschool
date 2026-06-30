5635067f49c6072b79527ae66d25a9e81592b1aa
K Uday Kiran <udaykirank74@gmail.com>
2026-07-01 01:32:07 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -33,3 +33,147 @@ function changeHeroImage(){
 }
 // Change every 5 seconds
 setInterval(changeHeroImage,5000);
+// Sticky Navbar
+
+const header=document.querySelector("header");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>80){
+
+header.classList.add("scrolled");
+
+}
+
+else{
+
+header.classList.remove("scrolled");
+
+}
+
+});
+// ===============================
+// Counter Animation
+// ===============================
+
+const counters = document.querySelectorAll(".counter");
+
+const counterObserver = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+const counter = entry.target;
+
+const target = +counter.dataset.target;
+
+let count = 0;
+
+const speed = target / 100;
+
+const updateCounter = ()=>{
+
+count += speed;
+
+if(count < target){
+
+counter.innerText = Math.ceil(count);
+
+requestAnimationFrame(updateCounter);
+
+}else{
+
+counter.innerText = target + "+";
+
+}
+
+};
+
+updateCounter();
+
+counterObserver.unobserve(counter);
+
+}
+
+});
+
+});
+
+counters.forEach(counter=>counterObserver.observe(counter));
+// ===============================
+// Back To Top
+// ===============================
+
+
+const backToTop = document.getElementById("backToTop");
+
+if(backToTop){
+
+    window.addEventListener("scroll",()=>{
+
+        if(window.scrollY>400){
+
+            backToTop.classList.add("show");
+
+        }else{
+
+            backToTop.classList.remove("show");
+
+        }
+
+    });
+
+    backToTop.addEventListener("click",()=>{
+
+        window.scrollTo({
+
+            top:0,
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+}
+// ===============================
+// Loading Screen
+// ===============================
+
+window.addEventListener("load", function () {
+
+    const loader = document.getElementById("loader");
+
+    if(loader){
+
+        setTimeout(function(){
+
+            loader.style.opacity = "0";
+
+            loader.style.visibility = "hidden";
+
+        },2000);
+
+    }
+
+});
+// ===============================
+// Scroll Progress Bar
+// ===============================
+
+const progressBar = document.getElementById("progress-bar");
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop = document.documentElement.scrollTop;
+
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    progressBar.style.width = progress + "%";
+
+});
